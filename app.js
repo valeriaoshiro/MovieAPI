19bb754ae5fde0ba1c23547d0acb128cfce57fa3
valeriaoshiro <valeriaoshiro19@gmail.com>
2018-02-06 16:06:57 -0800

    Display API

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ var express = require('express');
 var app = express();
 var request = require('request');
 
+app.set("view engine", "ejs");
+
 app.get('/results', function(req, res){
 	var url = 'http://omdbapi.com/?s=california&apikey=' + process.env.MOVIEKEY;
 	request(url, function(error, response, body){
 		if(!error && response.statusCode == 200){
-			res.send(body);
+			var data = JSON.parse(body);
+			res.render('results', {data:data});
 		}
 	});
 });
