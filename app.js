26cc25930e8d01d0c04e1bf7a230f22d74427187
valeriaoshiro <valeriaoshiro19@gmail.com>
2018-02-06 16:13:55 -0800

    Add search option

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ var request = require('request');
 
 app.set("view engine", "ejs");
 
+app.get('/', function(req, res){
+	res.render('search');
+});
+
 app.get('/results', function(req, res){
-	var url = 'http://omdbapi.com/?s=california&apikey=' + process.env.MOVIEKEY;
+	var search = req.query.search
+	var url = 'http://omdbapi.com/?s=' + search + '&apikey=' + process.env.MOVIEKEY;
 	request(url, function(error, response, body){
 		if(!error && response.statusCode == 200){
 			var data = JSON.parse(body);
